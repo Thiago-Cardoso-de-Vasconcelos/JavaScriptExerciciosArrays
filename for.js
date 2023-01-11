be51e2da50d1e56316b15760dea0566d4d3937d1
Thiago Cardoso de Vasconcelos <thiagocardosodevasconcelos@outlook.com>
2023-01-11 20:40:37 -0300

    .

--- for.js
+++ for.js
@@ -2,4 +2,4 @@ const listaDeNomes = ["Thiago", "Lucas", "Priscila", "Douglas", "João", "Ana"];
 
 for(let indice= 0; indice < listaDeNomes.length; indice++){
     console.log(listaDeNomes[indice]);
-};
+}
